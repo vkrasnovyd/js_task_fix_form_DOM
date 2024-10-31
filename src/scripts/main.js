48d28ba87ac0b4fbd41b56a17e2a2ac33d156d62
vkrasnovyd <v.krasnovyd@gmail.com>
2024-10-31 17:52:18 +0100

    Solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,31 @@
 'use strict';
 
-// write code here
+const inputFields = document.querySelectorAll('input');
+
+/**
+ * Decamelizes a string with a whitespace.
+ *
+ * @param str String in camelcase
+ */
+function decamelize(str) {
+  return str
+    .replace(/([a-z\d])([A-Z])/g, '$1 $2')
+    .replace(/([A-Z]+)([A-Z][a-z\d]+)/g, '$1 $2')
+    .toLowerCase()
+    .replace(/^./, (char) => char.toUpperCase());
+}
+
+for (const input of inputFields) {
+  const decamelizedInputName = decamelize(input.name);
+
+  // Create label element
+  const label = document.createElement('label');
+
+  label.className = 'field-label';
+  label.htmlFor = input.id;
+  label.textContent = decamelizedInputName;
+
+  // Modify DOM
+  input.before(label);
+  input.placeholder = decamelizedInputName;
+}
